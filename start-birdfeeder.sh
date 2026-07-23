b6c61d7b88af64774e4aafd6284de41aa5385915
Chris Hawthorne <78388764+KJ7PPK@users.noreply.github.com>
2026-07-22 20:39:54 -0700

    Update start-birdfeeder.sh
    removed "termux-brightness 0" - since switching to the service-type model rather than running in the foreground, this just serves as an annoyance constantly dimming the screen making physically inspecting the phone's status annoying.

--- start-birdfeeder.sh
+++ start-birdfeeder.sh
@@ -2,7 +2,6 @@
 export PATH=/data/data/com.termux/files/usr/bin:$PATH
 export HOME=/data/data/com.termux/files/home
 termux-wake-lock
-termux-brightness 0
 if ! pgrep -f "$HOME/birdfeeder.sh" > /dev/null; then
   nohup sh "$HOME/birdfeeder.sh" > "$HOME/birdfeeder.log" 2>&1 &
 fi
